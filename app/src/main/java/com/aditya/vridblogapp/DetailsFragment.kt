08e39335daf25e392ef4ec6e2046c892973f08a0
adityabhamare10 <adityabhamare71@gmail.com>
2024-06-09 13:00:47 +0530

    Content Fetched

--- app/src/main/java/com/aditya/vridblogapp/DetailsFragment.kt
+++ app/src/main/java/com/aditya/vridblogapp/DetailsFragment.kt
@@ -1,14 +1,17 @@
 package com.aditya.vridblogapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebSettings
 import android.webkit.WebView
+import android.webkit.WebViewClient
 
-public const val ARG_PARAM1 = "param1"
-public const val ARG_PARAM2 = "param2"
+const val ARG_PARAM1 = "param1"
+const val ARG_PARAM2 = "param2"
 
 class DetailsFragment : Fragment() {
     private var param1: String? = null
@@ -26,7 +29,7 @@ class DetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_details, container, false)
     }
 
@@ -34,12 +37,58 @@ class DetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val webView: WebView = view.findViewById(R.id.web_view)
-        val content = arguments?.getString("content")
+        var content = arguments?.getString("content")
 
-        webView.settings.javaScriptEnabled = true
-        webView.loadData(content ?: "", "text/html", "UTF-8")
+        Log.d("DetailsFragment", "Content received: $content")
+
+        content = """
+             <html>
+            <head>
+                <style>
+                    body {
+                        font-size: 35px;
+                        padding-left: 20px;
+                        padding-right: 20px;
+                        line-height: 1.6;
+                    }
+                    img {
+                        width: 100%;
+                        height: auto;
+                    }
+                </style>
+            </head>
+            <body>
+                ${content ?: ""}
+            </body>
+            </html>
+        """
+
+        webView.settings.apply {
+            javaScriptEnabled = true
+            loadWithOverviewMode = true
+            useWideViewPort = true
+            domStorageEnabled = true
+            defaultFontSize = 40
+        }
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                Log.d("DetailsFragment", "Page loaded: $url")
+            }
+
+            override fun onReceivedError(view: WebView?, errorCode: Int, description: String?, failingUrl: String?) {
+                super.onReceivedError(view, errorCode, description, failingUrl)
+                Log.e("DetailsFragment", "Error loading page: $description")
+            }
+        }
+
+        if (content != null) {
+            webView.loadDataWithBaseURL(null, content, "text/html", "UTF-8", null)
+        } else {
+            Log.e("DetailsFragment", "Content is null")
+        }
     }
-
 
     companion object {
         @JvmStatic
@@ -52,3 +101,4 @@ class DetailsFragment : Fragment() {
             }
     }
 }
+
